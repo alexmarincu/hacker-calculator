765ebc65a14cc86cfc9f73553b403f68f038ffbc
Alex Marincu <alex.marincu@gmail.com>
2023-11-30 02:40:20 +0200

    Title bar replaced with functionalities of the app bar; Added handling for assets compatible with pyinstaller; Center the window on open.

--- src/main.py
+++ src/main.py
@@ -1,81 +1,88 @@
+import sys
+import os
 import flet as ft
 import pyperclip as pc
 from math import *
 import webbrowser
 
-safeTokenList = [
-    # math
-    'e',
-    'pi',
-    'inf',
-    'nan',
-    'tau',
-    'acos',
-    'acosh',
-    'asin',
-    'asinh',
-    'atan',
-    'atan2',
-    'atanh',
-    'cbrt',
-    'ceil',
-    'comb',
-    'copysign',
-    'cos',
-    'cosh',
-    'degrees',
-    'dist',
-    'erf',
-    'erfc',
-    'exp',
-    'exp2',
-    'expm1',
-    'fabs',
-    'factorial',
-    'floor',
-    'fmod',
-    'frexp',
-    'fsum',
-    'gamma',
-    'gcd',
-    'hypot',
-    'isclose',
-    'isinf',
-    'isfinite',
-    'isnan',
-    'isqrt',
-    'lcm',
-    'ldexp',
-    'lgamma',
-    'log',
-    'log10',
-    'log1p',
-    'log2',
-    'modf',
-    'nextafter',
-    'perm',
-    'pow',
-    'prod',
-    'radians',
-    'remainder',
-    'sin',
-    'sinh',
-    'sumprod',
-    'sqrt',
-    'tan',
-    'tanh',
-    'trunc',
-]
-safeTokenDict = dict(
-    [(k, globals().get(k, None)) for k in safeTokenList]
-)
-safeTokenDict['abs'] = abs
-safeTokenDict['min'] = min
-safeTokenDict['max'] = max
-safeTokenDict['round'] = round
-
 
 def main(page: ft.Page) -> None:
+    safeTokenList = [
+        # math
+        'e',
+        'pi',
+        'inf',
+        'nan',
+        'tau',
+        'acos',
+        'acosh',
+        'asin',
+        'asinh',
+        'atan',
+        'atan2',
+        'atanh',
+        'cbrt',
+        'ceil',
+        'comb',
+        'copysign',
+        'cos',
+        'cosh',
+        'degrees',
+        'dist',
+        'erf',
+        'erfc',
+        'exp',
+        'exp2',
+        'expm1',
+        'fabs',
+        'factorial',
+        'floor',
+        'fmod',
+        'frexp',
+        'fsum',
+        'gamma',
+        'gcd',
+        'hypot',
+        'isclose',
+        'isinf',
+        'isfinite',
+        'isnan',
+        'isqrt',
+        'lcm',
+        'ldexp',
+        'lgamma',
+        'log',
+        'log10',
+        'log1p',
+        'log2',
+        'modf',
+        'nextafter',
+        'perm',
+        'pow',
+        'prod',
+        'radians',
+        'remainder',
+        'sin',
+        'sinh',
+        'sumprod',
+        'sqrt',
+        'tan',
+        'tanh',
+        'trunc',
+    ]
+    safeTokenDict = dict(
+        [(k, globals().get(k, None)) for k in safeTokenList]
+    )
+    safeTokenDict['abs'] = abs
+    safeTokenDict['min'] = min
+    safeTokenDict['max'] = max
+    safeTokenDict['round'] = round
+
+    def assetsPath(relativePath) -> str:
+        return os.path.join(
+            getattr(sys, '_MEIPASS', os.path.abspath('assets')), relativePath
+        )
+
     page.title = "Hacker Calculator"
     page.theme_mode = ft.ThemeMode.DARK
     page.window_min_width = 600
@@ -83,13 +90,17 @@ def main(page: ft.Page) -> None:
     page.window_width = 600
     page.window_height = 400
     page.window_always_on_top = True
+    page.window_title_bar_hidden = True
+    page.window_title_bar_buttons_hidden = True
     page.appbar = ft.AppBar(
         leading=ft.Container(
-            content=ft.Icon(ft.icons.CALCULATE, scale=1.5),
+            content=ft.Image(src=assetsPath("icon.png")),
             padding=ft.padding.only(left=20)
         ),
-        color=ft.colors.PRIMARY,
-        title=ft.Text("Hacker Calculator"),
+        bgcolor='#202324',
+        title=ft.WindowDragArea(
+            content=ft.Text("Hacker Calculator"), expand=True
+        ),
         actions=[
             ft.IconButton(
                 ft.icons.INFO,
@@ -98,11 +109,17 @@ def main(page: ft.Page) -> None:
                     'https://github.com/alexmarincu/hacker-calculator'
                 )
             ),
-            ft.VerticalDivider(width=20, color=ft.colors.TRANSPARENT)
+            ft.IconButton(
+                ft.icons.CLOSE,
+                on_click=lambda _: page.window_close()
+            ),
+            ft.VerticalDivider(width=15, color=ft.colors.TRANSPARENT),
+
         ],
     )
     page.fonts = {
-        "JetBrainsMono NF": "fonts/JetBrainsMonoNerdFont-Regular.ttf",
+        "JetBrainsMono NF":
+        assetsPath("fonts/JetBrainsMonoNerdFont-Regular.ttf"),
     }
     page.theme = ft.Theme(font_family="JetBrainsMono NF")
     expressionTextField = ft.Ref[ft.TextField]()
@@ -110,7 +127,7 @@ def main(page: ft.Page) -> None:
     resultHexTextButton = ft.Ref[ft.TextButton]()
     resultBinaryTextButton = ft.Ref[ft.TextButton]()
 
-    def on_keyboard(ev: ft.KeyboardEvent):
+    def onKey(ev: ft.KeyboardEvent):
         if ev.key == "Escape":
             if expressionTextField.current.value != "":
                 expressionTextField.current.value = ""
@@ -119,7 +136,7 @@ def main(page: ft.Page) -> None:
                 expressionTextField.current.focus()
             else:
                 page.window_close()
-    page.on_keyboard_event = on_keyboard
+    page.on_keyboard_event = onKey
 
     def onResultClick(ev: ft.ControlEvent) -> None:
         pc.copy(ev.control.text)
@@ -199,8 +216,9 @@ def main(page: ft.Page) -> None:
             )
         )
     )
+    page.window_center()
     expressionTextField.current.focus()
 
 
 if __name__ == "__main__":
-    ft.app(target=main, assets_dir="../assets")
+    ft.app(target=main)
